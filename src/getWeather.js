51b3b8a526f03616124ed7fad204892d0a2952a3
MeeseGod <63038587+MeeseGod@users.noreply.github.com>
2021-12-03 01:23:54 -0500

    Update getWeather.js

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,5 +1,5 @@
 async function getWeather(){
-    const getWeather = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=", {mode: "cors"});
+    const getWeather = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=82af43cca434fb2c7e0d2c0195f824fd", {mode: "cors"});
     const weatherData = await getWeather.json();
     console.log(weatherData);
     console.log((weatherData.main.temp - 273).toFixed(0));
