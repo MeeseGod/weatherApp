248cb983d9261f6347c43cad932f124678eaf0cc
JosephR <joseph_rundell@outlook.com>
2021-12-04 15:56:34 -0500

    Added styling etc.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,14 +1,18 @@
+const tempDisplay = document.getElementById("tempDisplay");
+const cityName = document.getElementById("cityName");
+
 async function getWeather(city){
     try{
         const getWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=82af43cca434fb2c7e0d2c0195f824fd`, {mode: "cors"});
         const weatherData = await getWeather.json();
         console.log(weatherData);
         console.log(`${city}'s Temperature: ` + (weatherData.main.temp - 273).toFixed(0));
-        document.querySelector("h2").textContent = (weatherData.main.temp - 273).toFixed(0);
+        cityName.textContent = city;
+        tempDisplay.textContent = (weatherData.main.temp - 273).toFixed(0);
     }
     catch{
         console.log("Error")
-        document.querySelector("h2").textContent = "Error";
+        tempDisplay.textContent = "Error";
     }
 }
 
