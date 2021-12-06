ee8688973b4dfa14c60df8ab3f0a0dbdb59cfafa
JosephR <joseph_rundell@outlook.com>
2021-12-06 05:00:22 -0500

    Updated Styling | Updated conversion method for temps

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,18 +1,36 @@
 const tempDisplay = document.getElementById("tempDisplay");
 const cityName = document.getElementById("cityName");
+const feelsLike = document.getElementById("feelsLike");
+const mainDescription = document.getElementById("mainDescription");
+const tempMin = document.getElementById("tempMin");
+const tempMax = document.getElementById("tempMax");
+let tempSetting = "C";
 
 async function getWeather(city){
     try{
         const getWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=82af43cca434fb2c7e0d2c0195f824fd`, {mode: "cors"});
         const weatherData = await getWeather.json();
         console.log(weatherData);
-        console.log(`${city}'s Temperature: ` + (weatherData.main.temp - 273).toFixed(0));
+        console.log(`${city}'s Temperature: ` + conversion(weatherData.main.temp, tempSetting));
         cityName.textContent = city;
-        tempDisplay.textContent = (weatherData.main.temp - 273).toFixed(0);
+        tempDisplay.textContent = conversion(weatherData.main.temp, tempSetting) + `°${tempSetting}`;
+        feelsLike.textContent = "Feels like: " + conversion(weatherData.main.feels_like, tempSetting) + `°${tempSetting}`;
+        tempMin.textContent = "Minimum Temperature: " + conversion(weatherData.main.temp_min, tempSetting) + `°${tempSetting}`;
+        tempMax.textContent = "Maximum Temperature: " + conversion(weatherData.main.temp_max, tempSetting) + `°${tempSetting}`;
+        mainDescription.textContent = weatherData.weather[0].description;
     }
     catch{
         console.log("Error")
         tempDisplay.textContent = "Error";
+    }
+}
+
+function conversion(temperature, tempSetting){
+    if(tempSetting == "C"){
+        return (temperature - 273).toFixed(0)
+    }
+    else if(tempSetting == "F"){
+        return (temperature - 459.67).toFixed(0);
     }
 }
 
